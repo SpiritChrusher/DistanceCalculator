2c987d695ffc979b937c2ab508e58df992eaf841
Kristof Szabo <Kristof.Szabo@logmein.com>
2022-09-13 02:18:39 +0200

    UI elements and basic logic added

--- DistanceCalculator/MainPage.xaml.cs
+++ DistanceCalculator/MainPage.xaml.cs
@@ -1,24 +1,66 @@
-﻿namespace DistanceCalculator;
+﻿using System.Diagnostics;
+
+namespace DistanceCalculator;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	Stopwatch clock;
 
-	public MainPage()
+
+    public MainPage()
 	{
 		InitializeComponent();
+		clock = new Stopwatch();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void OnTimerClicked(object sender, EventArgs e)
 	{
-		count++;
+        if (Accelerometer.Default.IsSupported)
+        {
+            if (!Accelerometer.Default.IsMonitoring)
+            {
+                // Turn on accelerometer
+                Accelerometer.Default.ReadingChanged += Accelerometer_ReadingChanged;
+                Accelerometer.Default.Start(SensorSpeed.UI);
+            }
+        }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
+        count++;
+        clock.Start();
+        if (count%2 is 0)
+		{
+            clock.Stop();
+            TimerBtn.Text = $"Resume";
+            ShowTime.Text = clock.Elapsed.ToString();
+        }
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+		{
+            clock.Start();
+            TimerBtn.Text = $"Stop";
+            ShowTime.Text = clock.Elapsed.ToString();
+        }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+        SemanticScreenReader.Announce(TimerBtn.Text);
 	}
+
+    private void OnStopClicked(object sender, EventArgs e)
+    {
+        clock.Reset();
+        TimerBtn.Text = $"Start";
+
+        // Turn off accelerometer
+        //Accelerometer.Default.Stop();
+        //Accelerometer.Default.ReadingChanged -= Accelerometer_ReadingChanged;
+
+        SemanticScreenReader.Announce(TimerBtn.Text);
+    }
+
+    private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+    {
+        // Update UI Label with accelerometer state
+        AccelLabel.TextColor = Colors.Green;
+        AccelLabel.Text = $"Accel: {e.Reading}";
+    }
 }
 
